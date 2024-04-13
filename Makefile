96d6ccc35aacdba93bbd0c611a05fff8cf419e0e
robert <robert@arles.us>
2024-04-13 10:21:18 -0700

    save state

--- Makefile
+++ Makefile
@@ -2,19 +2,14 @@ SHELL := /bin/bash
 ## Makefile for setting up a new machine
 ## 	Use '##' after target to document the target, for CLI help
 ##   e.g. targetname: ## CLI help text for this target
+
 help:               ## Show this help.
 	@egrep -h '\s##\s' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m  %-30s\033[0m %s\n", $$1, $$2}'
 # allow for commands that are not really building anything
-.PHONY: buildwin buildlinux buildmacarm64 buildmacamd64 buildall
-buildmacamd64: ## build a mac intel binary
-	GOOS=darwin GOARCH=amd64 go build -o bin/macamd64/hoist hoist/main.go
-buildmacarm64: ## build a mac arm64 binary 
-	GOOS=darwin GOARCH=arm64 go build -o bin/macarm64/hoist hoist/main.go
-buildlinux: ## build a linux binary
-	GOOS=linux GOARCH=amd64 go build -o bin/linux/hoist hoist/main.go
-buildwin: ## build a windows binary
-	GOOS=windows GOARCH=amd64 go build -o bin/windows/hoist.exe hoist/main.go
-buildall: buildwin buildlinux buildmacarm64 buildmacamd64 ## build all binaries
-builddebug:
-	go build -o bin/debug/hoist hoist/main.go
+.PHONY: help buildlocal installlocal
 
+buildlocal: ## build the app
+  cd hoist; go build -o bin/hoist -ldflags "-X main.Version=$(cat Version.go | grep 'const Version' | cut -d' ' -f4)" main.go; cd ..
+
+installlocal: ## install the app
+  cd hoist; go install -ldflags "-X main.Version=$(cat Version.go | grep 'const Version' | cut -d' ' -f4)"; cd ..
--- hoist/main.go
+++ hoist/main.go
@@ -1,92 +1,133 @@
 package main
 
 import (
-    "crypto/sha256"
-    "encoding/hex"
-    "fmt"
-    "io"
-    "os"
-    "path/filepath"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 )
 
 // Function to calculate the SHA256 hash of a file
 func calculateHash(filePath string) (string, error) {
-    file, err := os.Open(filePath)
-    if err != nil {
-        return "", err
-    }
-    defer file.Close()
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
 
-    hasher := sha256.New()
-    if _, err := io.Copy(hasher, file); err != nil {
-        return "", err
-    }
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		return "", err
+	}
 
-    return hex.EncodeToString(hasher.Sum(nil)), nil
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+func isSymlink(filePath string) (bool, error) {
+	fileInfo, err := os.Lstat(filePath)
+	if err != nil {
+		return false, fmt.Errorf("failed stat: %w", err)
+	}
+	return fileInfo.Mode()&os.ModeSymlink == os.ModeSymlink, nil
 }
 
 // Function to recursively scan a directory and identify duplicate files
 func scanDirectory(rootDir string) (map[string][]string, error) {
-    fileHashes := make(map[string][]string)
+	fileHashes := make(map[string][]string)
 
-    err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if !info.IsDir() {
-            hash, err := calculateHash(path)
-            if err != nil {
-                return err
-            }
-            fileHashes[hash] = append(fileHashes[hash], path)
-        }
-        return nil
-    })
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// skip all links
+		if isLink, _ := isSymlink(path); !isLink {
+			// skip directories
+			if !info.IsDir() {
+				hash, err := calculateHash(path)
+				if err != nil {
+					return err
+				}
+				fileHashes[hash] = append(fileHashes[hash], path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
 
-    if err != nil {
-        return nil, err
-    }
-
-    return fileHashes, nil
+	return fileHashes, nil
 }
 
 // Function to hoist duplicate files and create links to hoisted files
-func hoistDuplicates(fileHashes map[string][]string) error {
-    for _, paths := range fileHashes {
-        if len(paths) > 1 {
-            hoistedFilePath := paths[0] + ".hoisted"
-            if err := os.Rename(paths[0], hoistedFilePath); err != nil {
-                return err
-            }
-            for _, linkPath := range paths[1:] {
-                if err := os.Link(hoistedFilePath, linkPath); err != nil {
-                    return err
-                }
-            }
-        }
-    }
-    return nil
+func hoistFiles(fileHashes map[string][]string, rootDir string) error {
+	// create a directory in the root directory to store the hoisted files
+	hoistDirName := filepath.Join(rootDir, "hoisted-resources")
+	for fileHash, paths := range fileHashes {
+		if len(paths) > 1 {
+			for _, originalPath := range paths[1:] {
+				hoistHashedPath := filepath.Join(hoistDirName, fileHash)
+				fmt.Println("Moving:", originalPath, hoistHashedPath)
+				// create the target directory for the hoisted file
+				if err := os.MkdirAll(filepath.Dir(hoistHashedPath), 0755); err != nil {
+					return err
+				}
+				// move the file to the hoisted location
+				if err := os.Rename(originalPath, hoistHashedPath); err != nil {
+					return err
+				}
+				// get the relative path from the original file location, to the hoistPath
+				relHoistedPath, err := filepath.Rel(filepath.Dir(originalPath), hoistHashedPath)
+				if err != nil {
+					return err
+				}
+				if err := os.Symlink(relHoistedPath, originalPath); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func printHelp() {
+	fmt.Println("Hoist")
+	fmt.Println("\tA tool to hoist duplicate files and create links to hoisted files.")
+	fmt.Println("\t- Works relative to the workind directory, moving dupes to `hoisted-resources` directory and replaceing the originals with links.")
+	fmt.Println("Version:", Version)
+	fmt.Println("Usage: hoist <rootDir>")
 }
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("Usage: ./program <root_directory>")
-        os.Exit(1)
-    }
+	if len(os.Args) != 2 {
+		printHelp()
+		os.Exit(1)
+	}
 
-    rootDir := os.Args[1]
+	if os.Args[1] == "--help" || os.Args[1] == "-h" {
+		printHelp()
+		os.Exit(0)
+	}
+	if os.Args[1] == "--version" || os.Args[1] == "-v" {
+		fmt.Println("Version:", Version)
+		os.Exit(0)
+	}
 
-    fileHashes, err := scanDirectory(rootDir)
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
+	rootDir := os.Args[1]
 
-    if err := hoistDuplicates(fileHashes); err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
+	fileHashes, err := scanDirectory(rootDir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 
-    fmt.Println("Duplicate files hoisted and links created successfully.")
+	if err := hoistFiles(fileHashes, rootDir); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Duplicate files hoisted and links created successfully.")
+	fmt.Println("Version:", Version)
 }
-
